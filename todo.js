40cd13c85bcd0365a3bc655582c50f4847a06bf6
kkongchi <smallbean99s@gmail.com>
2023-04-29 02:16:07 +0900

    first commit

---
+++ todo.js
@@ -0,0 +1,42 @@
+
+const todo = document.getElementById("todoList");
+const todoForm = document.querySelector('form');
+
+todoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const taskInput = event.target['task'];
+    const detailTask = document.createElement('div');
+    detailTask.setAttribute("class", "detailTask");
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", 'checkbox');
+    checkbox.setAttribute("class", "checkbox")
+
+    const taskName = document.createElement("span");
+    taskName.innerText = taskInput.value;
+    taskName.setAttribute("class", "taskName")
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.innerText = "삭제";
+
+    detailTask.appendChild(checkbox);
+    detailTask.appendChild(taskName);
+    detailTask.appendChild(deleteButton);
+    todo.appendChild(detailTask );
+
+
+    checkbox.addEventListener('change', event =>{
+        if(checkbox.checked){
+            taskName.style.textDecorationLine = "line-through"
+        }else{
+            taskName.style.textDecorationLine = "none"
+        }
+    });
+    
+    deleteButton.addEventListener('click', event =>{
+        event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode);
+    });
+});
+
